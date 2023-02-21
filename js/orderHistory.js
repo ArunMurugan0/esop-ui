d6bc1505ba75d5c48ca4d57386a5aee5ab21d701
Aditya Kedar Tiwari <aditakedart@Adityas-MacBook-Pro.local>
2023-02-21 23:17:14 +0530

    Modify: Success reponse is added in table format for orderHistory

--- js/orderHistory.js
+++ js/orderHistory.js
@@ -39,6 +39,20 @@ function errorHandlerForOrderHistory(errResponse) {
     errorContainer.append(errorListContainer)
 }
 
+function createHeaders(table){
+    const tr = table.insertRow()
+    const headers = ["Order ID","Order Type", "Order Quantity", "Order Price", "Order Status","Order Execution History", "Remaining Quantity"]
+    for(let i = 0; i < headers.length; i++){
+        const td = tr.insertCell()
+        td.appendChild(document.createTextNode(headers[i]))
+    }
+}
+
+function addDataToRow(tr,data){
+    const td = tr.insertCell()
+    td.appendChild(document.createTextNode(String(data)))
+}
+
 function successHandlerForOrderHistory(response){
     const ressponseContainer = document.getElementById("success-response-container")
     while (ressponseContainer.hasChildNodes()){
@@ -50,7 +64,38 @@ function successHandlerForOrderHistory(response){
         ressponseContainer.append(message)
     }
     else{
+        const table = document.createElement("table")
+        createHeaders(table)
+        for(let i = 0; i < response.length; i++){
+            const tr = table.insertRow()
+            const quantity = response[i]["quantity"]
+            addDataToRow(tr,response[i]["orderId"])
+            addDataToRow(tr,quantity)
+            addDataToRow(tr,response[i]["type"])
+            addDataToRow(tr,response[i]["price"])
+            addDataToRow(tr,response[i]["status"])
+            const filledOrders = response[i]["filled"]
+            const td = tr.insertCell()
+            let remainingQuantity = quantity
+            for(let j = 0; j < filledOrders.length; j++){
+                const filledOrderTable = document.createElement("table")
+                const filledOrderTableHeaders = ["Quantity", "Price"]
+                const filledOrderTableHeaderRow = filledOrderTable.insertRow()
+                for(let k = 0; k < filledOrderTableHeaders.length; k++){
+                    const td1 = filledOrderTableHeaderRow.insertCell()
+                    td1.appendChild(document.createTextNode(filledOrderTableHeaders[k]))
+                }
+                const filledOrderTableDataRow = filledOrderTable.insertRow()
+                const executedQuantity = filledOrders[j]["quantity"]
+                remainingQuantity = remainingQuantity - executedQuantity
+                addDataToRow(filledOrderTableDataRow, executedQuantity)
+                addDataToRow(filledOrderTableDataRow, filledOrders[j]["amount"])
+                td.appendChild(filledOrderTable)
+            }
+            addDataToRow(tr,remainingQuantity)
+        }
 
+        ressponseContainer.append(table)
     }
 }
 
